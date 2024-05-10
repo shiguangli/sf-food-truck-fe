c1efdbfa931480bcff91d377dc51175a9bc34de1
ShiguangLi <lishiguang@outlook.com>
2024-05-10 17:45:29 +0800

    v1.0.0 call real API

--- src/App.js
+++ src/App.js
@@ -2,10 +2,6 @@ import { useState, useEffect } from "react"
 import axios from "axios";
 import { Table, Input } from 'antd';
 
-// TODO: get data from API
-import foodTruckList from "./mobile_food_truck.json"
-import searchResult from './search_result.json'
-
 import './App.css';
 
 const { Search } = Input;
@@ -13,7 +9,8 @@ const { Search } = Input;
 function App() {
   const [loading, setLoading] = useState(false);
   const [err, setErr] = useState(null);
-  let [list, setList] = useState(foodTruckList)
+  const [searchText, setSearchText] = useState("")
+  let [list, setList] = useState([])
   
   const columns = [{"title":"locationid","dataIndex":"locationid","key":"locationid"},{"title":"Applicant","dataIndex":"Applicant","key":"applicant"},{"title":"FacilityType","dataIndex":"FacilityType","key":"facilitytype"},{"title":"cnn","dataIndex":"cnn","key":"cnn"},{"title":"LocationDescription","dataIndex":"LocationDescription","key":"locationdescription"},{"title":"Address","dataIndex":"Address","key":"address"},{"title":"blocklot","dataIndex":"blocklot","key":"blocklot"},{"title":"block","dataIndex":"block","key":"block"},{"title":"lot","dataIndex":"lot","key":"lot"},{"title":"permit","dataIndex":"permit","key":"permit"},{"title":"Status","dataIndex":"Status","key":"status"},{"title":"FoodItems","dataIndex":"FoodItems","key":"fooditems"},{"title":"X","dataIndex":"X","key":"x"},{"title":"Y","dataIndex":"Y","key":"y"},{"title":"Latitude","dataIndex":"Latitude","key":"latitude"},{"title":"Longitude","dataIndex":"Longitude","key":"longitude"},{"title":"Schedule","dataIndex":"Schedule","key":"schedule"},{"title":"dayshours","dataIndex":"dayshours","key":"dayshours"},{"title":"NOISent","dataIndex":"NOISent","key":"noisent"},{"title":"Approved","dataIndex":"Approved","key":"approved"},{"title":"Received","dataIndex":"Received","key":"received"},{"title":"PriorPermit","dataIndex":"PriorPermit","key":"priorpermit"},{"title":"ExpirationDate","dataIndex":"ExpirationDate","key":"expirationdate"},{"title":"Location","dataIndex":"Location","key":"location"},{"title":"Fire Prevention Districts","dataIndex":"Fire Prevention Districts","key":"fire_prevention_districts"},{"title":"Police Districts","dataIndex":"Police Districts","key":"police_districts"},{"title":"Supervisor Districts","dataIndex":"Supervisor Districts","key":"supervisor_districts"},{"title":"Zip Codes","dataIndex":"Zip Codes","key":"zip_codes"},{"title":"Neighborhoods (old)","dataIndex":"Neighborhoods (old)","key":"neighborhoods_old"}]
 
@@ -21,8 +18,8 @@ function App() {
     const fetchData = async () => {
       setLoading(true);
       try {
-        // const searchResult = searchByAPI()
-        // setList(searchResult)
+        let lst = await searchByAPI('')
+        setList(lst)
       } catch (error) {
         setErr(error);
       }
@@ -32,14 +29,18 @@ function App() {
     fetchData();
   }, []);
 
+  const handleSearchTextChange = (e) => {
+    setSearchText(e.target.value);
+  };
+
   const searchByAPI = async (keyword) => {
-    // const response = await axios.get(`http://localhost:5173/api/food_truck_search?keyword=${keyword}`);
+    const response = await axios.get(`http://localhost:3001/food-truck/search?input=${keyword}`);
+    console.log(`API RESP -- ${response}`)
     // return response
-    return searchResult
+    return response?.data || []
   }
 
   const searchByKeywords = async (keyword) => {
-    // TODO: API call
     let r = await searchByAPI(keyword)
     setList(r)
     // setPageNumber(1)
@@ -49,7 +50,7 @@ function App() {
     <div className="app-container">
       <div className="app-area">
         <div className="input-area">
-          <Search placeholder="Enter Keywords Here" enterButton="Search" onSearch={searchByKeywords} size="large" loading={loading} />
+          <Search placeholder="Enter Keywords Here" enterButton="Search" value={searchText} onChange={handleSearchTextChange} onSearch={searchByKeywords} size="large" loading={loading} />
         </div>
         <div className="output-area">
           <Table columns={columns} dataSource={list} 
