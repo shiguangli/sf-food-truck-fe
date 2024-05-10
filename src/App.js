58959751194f67754c12181b5ad351dcc7d83753
ShiguangLi <lishiguang@outlook.com>
2024-05-10 13:30:17 +0800

    delete unused code

--- src/App.js
+++ src/App.js
@@ -12,12 +12,9 @@ const { Search } = Input;
 
 function App() {
   const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null);
+  const [err, setErr] = useState(null);
   let [list, setList] = useState(foodTruckList)
-  // let [pageNumber, setPageNumber] = useState(1)
-  // let listView = list.map((item, index) => {
-  //   return <div key={index+10}>{item.optionaltext}</div>
-  // })
+  
   const columns = [{"title":"locationid","dataIndex":"locationid","key":"locationid"},{"title":"Applicant","dataIndex":"Applicant","key":"applicant"},{"title":"FacilityType","dataIndex":"FacilityType","key":"facilitytype"},{"title":"cnn","dataIndex":"cnn","key":"cnn"},{"title":"LocationDescription","dataIndex":"LocationDescription","key":"locationdescription"},{"title":"Address","dataIndex":"Address","key":"address"},{"title":"blocklot","dataIndex":"blocklot","key":"blocklot"},{"title":"block","dataIndex":"block","key":"block"},{"title":"lot","dataIndex":"lot","key":"lot"},{"title":"permit","dataIndex":"permit","key":"permit"},{"title":"Status","dataIndex":"Status","key":"status"},{"title":"FoodItems","dataIndex":"FoodItems","key":"fooditems"},{"title":"X","dataIndex":"X","key":"x"},{"title":"Y","dataIndex":"Y","key":"y"},{"title":"Latitude","dataIndex":"Latitude","key":"latitude"},{"title":"Longitude","dataIndex":"Longitude","key":"longitude"},{"title":"Schedule","dataIndex":"Schedule","key":"schedule"},{"title":"dayshours","dataIndex":"dayshours","key":"dayshours"},{"title":"NOISent","dataIndex":"NOISent","key":"noisent"},{"title":"Approved","dataIndex":"Approved","key":"approved"},{"title":"Received","dataIndex":"Received","key":"received"},{"title":"PriorPermit","dataIndex":"PriorPermit","key":"priorpermit"},{"title":"ExpirationDate","dataIndex":"ExpirationDate","key":"expirationdate"},{"title":"Location","dataIndex":"Location","key":"location"},{"title":"Fire Prevention Districts","dataIndex":"Fire Prevention Districts","key":"fire_prevention_districts"},{"title":"Police Districts","dataIndex":"Police Districts","key":"police_districts"},{"title":"Supervisor Districts","dataIndex":"Supervisor Districts","key":"supervisor_districts"},{"title":"Zip Codes","dataIndex":"Zip Codes","key":"zip_codes"},{"title":"Neighborhoods (old)","dataIndex":"Neighborhoods (old)","key":"neighborhoods_old"}]
 
   useEffect(() => {
@@ -27,7 +24,7 @@ function App() {
         // const searchResult = searchByAPI()
         // setList(searchResult)
       } catch (error) {
-        setError(error);
+        setErr(error);
       }
       setLoading(false);
     };
